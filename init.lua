0c366fb7da004c477609a66b05a7980715f2e43c
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-19 21:47:05 +0300

    feat: initializing file for NeoVim

---
+++ init.lua
@@ -0,0 +1,12 @@
+require("settings")
+require("plugins")
+require('keymaps')
+require("telescope/settings")
+require("plugin_config/neovimtree")
+require("plugin_config/tagbar")
+require("plugin_config/commenter")
+require("plugin_config/ale_settings")
+require("plugin_config/lsp")
+require("plugin_config/git")
+
+vim.cmd [[ colorscheme gruvbox ]]
