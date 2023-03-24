2e15d8c5504bbef7e7543ff086b48f43bfcb33ab
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-24 13:50:13 +0300

    feat(plugins): install Mason plugin

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -27,7 +27,9 @@ return require('packer').startup(function(use)
     'hrsh7th/cmp-nvim-lsp',
     'saadparwaiz1/cmp_luasnip',
     'L3MON4D3/LuaSnip',
-    'ray-x/lsp_signature.nvim'
+    'ray-x/lsp_signature.nvim',
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim"
 }
   
   -- NeoVim Tree
