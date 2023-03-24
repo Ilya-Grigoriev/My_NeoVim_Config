b5cc11c1d3d9b89095a5ef49285e2770e5dfc169
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-24 14:59:04 +0300

    feat(init): add plugin for surround_text

--- init.lua
+++ init.lua
@@ -9,5 +9,6 @@ require("plugin_config/ale_settings")
 require("plugin_config/lsp")
 require("plugin_config/git")
 require('plugin_config/webdevicons')
+require('plugin_config/surround_text')
 
 vim.cmd [[ colorscheme gruvbox ]]
