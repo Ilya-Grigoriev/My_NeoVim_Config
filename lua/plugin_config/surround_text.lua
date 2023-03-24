69e89fee405af1d07939cb08407a6e38f59a91ad
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-24 14:52:35 +0300

    feat(plugin_config): rewrite keymap for surrounding text

--- lua/plugin_config/surround_text.lua
+++ lua/plugin_config/surround_text.lua
@@ -1,9 +1,4 @@
-use({
-    "kylechui/nvim-surround",
-    tag = "*", -- Use for stability; omit to use `main` branch for the latest features
-    config = function()
-        require("nvim-surround").setup({
-            -- Configuration here, or leave empty to use defaults
-        })
-    end
-})
+vim.api.nvim_set_keymap('v', 'yw"', 'c"<Esc>p', {})
+vim.api.nvim_set_keymap('v', "yw'", "c'<Esc>p", {})
+vim.api.nvim_set_keymap('v', 'yw(', 'c(<Esc>p', {})
+vim.api.nvim_set_keymap('v', 'yw<', 'c<<Esc>pa>', {})
