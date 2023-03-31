9f4f707310919a905c1ac354b48d54f3a56fc8d4
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-31 15:26:52 +0300

    feat(plugin_config): removed option -a by default in git.lua

--- lua/plugin_config/git.lua
+++ lua/plugin_config/git.lua
@@ -1,8 +1,8 @@
 vim.api.nvim_set_keymap("n", ",g", ":!git ", {})
 vim.api.nvim_set_keymap('n', ',ga', ':!git add ', {})
-vim.api.nvim_set_keymap('n', ',gc', ':!git commit -a ', {})
+vim.api.nvim_set_keymap('n', ',gc', ':!git commit ', {})
 vim.api.nvim_set_keymap('n', ',gs', ':!git status<CR>', {})
-vim.api.nvim_set_keymap('n', ',gco', ':!git chekcout ', {}) 
+vim.api.nvim_set_keymap('n', ',gco', ':!git chekcout ', {})
 vim.api.nvim_set_keymap('n', ',gl', ':!git log<CR>', {})
 
 vim.cmd[[
