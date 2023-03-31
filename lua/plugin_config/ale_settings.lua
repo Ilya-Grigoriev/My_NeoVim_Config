b3b40e04a9b76c3eaead8a484ec7573638d0709b
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-31 21:57:37 +0300

    feat(plugin_config): disabled border signs

--- lua/plugin_config/ale_settings.lua
+++ lua/plugin_config/ale_settings.lua
@@ -8,3 +8,10 @@ g.ale_fix_on_save = 1
 g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
 g.ale_warn_about_trailing_whitespace = 0
 g.ale_virtualtext_cursor = 'disabled'
+g.ale_sign_column_always = 0
+vim.cmd[[
+highlight clear ALEErrorSign
+highlight clear ALEWarningSign
+]]
+g.ale_sign_error = ''
+g.ale_sign_warning = ''
--- lua/plugin_config/lsp.lua
+++ lua/plugin_config/lsp.lua
@@ -153,5 +153,6 @@ end
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
   vim.lsp.diagnostic.on_publish_diagnostics, {
     virtual_text = false,
+    signs = false
   }
 )
