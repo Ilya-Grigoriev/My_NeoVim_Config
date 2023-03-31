339181e6f70b2ac6bf46bbabc3cb8fdf52107633
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-03-31 15:27:49 +0300

    feat(plugin_config): added keymap for git push

--- lua/plugin_config/git.lua
+++ lua/plugin_config/git.lua
@@ -4,6 +4,7 @@ vim.api.nvim_set_keymap('n', ',gc', ':!git commit ', {})
 vim.api.nvim_set_keymap('n', ',gs', ':!git status<CR>', {})
 vim.api.nvim_set_keymap('n', ',gco', ':!git chekcout ', {})
 vim.api.nvim_set_keymap('n', ',gl', ':!git log<CR>', {})
+vim.api.nvim_set_keymap('n', ',gp', ':!git push<CR>', {})
 
 vim.cmd[[
 let g:gitblame_enabled = 0
