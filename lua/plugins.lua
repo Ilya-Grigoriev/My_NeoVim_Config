4683814c88ee662978911efe15ed547596f70eba
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-04-08 19:36:27 +0300

    refactor(plugins): added indents, etc

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -4,47 +4,47 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
 
-  --Telescope
-  use {
-          'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    --Telescope
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
         -- or                            , branch = '0.1.x',
-          requires = { {'nvim-lua/plenary.nvim'} }
+        requires = { {'nvim-lua/plenary.nvim'} }
     }
 
-  -- JS/JSX/TS
-  use {
-    'jose-elias-alvarez/null-ls.nvim',
-    'nvim-lua/plenary.nvim'
-}
+    -- JS/JSX/TS
+    use {
+        'jose-elias-alvarez/null-ls.nvim',
+        'nvim-lua/plenary.nvim'
+    }
 
-  -- LSP
-  use {
-    'neovim/nvim-lspconfig',
-    'hrsh7th/nvim-cmp',
-    'hrsh7th/cmp-nvim-lsp',
-    'ray-x/lsp_signature.nvim',
-    "williamboman/mason.nvim",
-    "williamboman/mason-lspconfig.nvim",
-}
-  
-  -- NeoVim Tree
-  use {"nvim-tree/nvim-tree.lua"}
+    -- LSP
+    use {
+        'neovim/nvim-lspconfig',
+        'hrsh7th/nvim-cmp',
+        'hrsh7th/cmp-nvim-lsp',
+        'ray-x/lsp_signature.nvim',
+        "williamboman/mason.nvim",
+        "williamboman/mason-lspconfig.nvim",
+    }
 
-  -- TagBar
-  use {'preservim/tagbar'}
+    -- NeoVim Tree
+    use {"nvim-tree/nvim-tree.lua"}
+
+    -- TagBar
+    use {'preservim/tagbar'}
 
     -- Themes
     use {'morhetz/gruvbox'}
     -- Commenter
     use {
-    'numToStr/Comment.nvim',
-    config = function()
-      require('Comment').setup()
-    end
-  }
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
 
     -- AutoPairs
     use {
@@ -81,15 +81,15 @@ return require('packer').startup(function(use)
 
     -- Terminal
     use {"akinsho/toggleterm.nvim", tag = '*', config = function()
-      require("toggleterm").setup()
+        require("toggleterm").setup()
     end}
 
     -- Git
     use {
-      'lewis6991/gitsigns.nvim',
-      config = function()
-        require('gitsigns').setup()
-      end
+        'lewis6991/gitsigns.nvim',
+        config = function()
+            require('gitsigns').setup()
+        end
     }
 
     -- Multi Cursor
