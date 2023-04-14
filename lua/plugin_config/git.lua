897a80bf77f07c5c531479e36eeb756208fcb08a
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-04-14 20:28:31 +0300

    feat(lua/plugin_config): added Lazygit plugin config

--- lua/plugin_config/git.lua
+++ lua/plugin_config/git.lua
@@ -7,3 +7,12 @@ vim.api.nvim_set_keymap('n', ',gl', ':!git log<CR>', {})
 vim.api.nvim_set_keymap('n', ',gp', ':!git push<CR>', {})
 vim.api.nvim_set_keymap('n', ',gb', ':!git branch<CR>', {})
 vim.api.nvim_set_keymap('n', ',gm', ':!git merge ', {})
+local Terminal  = require('toggleterm.terminal').Terminal
+local lazygit = Terminal:new({ cmd = "lazygit", count = 5 })
+-- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+
+function _lazygit_toggle()
+  lazygit:toggle()
+end
+
+vim.api.nvim_set_keymap("n", ",gg", "<cmd>lua _lazygit_toggle()<CR>", {noremap = true, silent = true})
