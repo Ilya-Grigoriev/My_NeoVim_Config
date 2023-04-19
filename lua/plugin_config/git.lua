f6f86a27e546557f2e3a0624d667eaf012b9f639
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-04-19 20:11:59 +0300

    feat(plugin_config/git): changed config for LazyGit Terminal

--- lua/plugin_config/git.lua
+++ lua/plugin_config/git.lua
@@ -8,8 +8,23 @@ vim.api.nvim_set_keymap('n', ',gp', ':!git push<CR>', {})
 vim.api.nvim_set_keymap('n', ',gb', ':!git branch<CR>', {})
 vim.api.nvim_set_keymap('n', ',gm', ':!git merge ', {})
 local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", count = 5 })
--- local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+local lazygit = Terminal:new({
+  cmd = "lazygit",
+  dir = "git_dir",
+  direction = "float",
+  float_opts = {
+    border = "double",
+  },
+  -- function to run on opening the terminal
+  on_open = function(term)
+    vim.cmd("startinsert!")
+    vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", {noremap = true, silent = true})
+  end,
+  -- function to run on closing the terminal
+  on_close = function(term)
+    vim.cmd("startinsert!")
+  end,
+})
 
 function _lazygit_toggle()
   lazygit:toggle()
