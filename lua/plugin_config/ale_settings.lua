934fae64fa3346632562d2e2523c89a89ce89b58
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-04-27 18:51:09 +0300

    feat(plugin_config/ale_settings): removed mypy from LSP linters

--- lua/plugin_config/ale_settings.lua
+++ lua/plugin_config/ale_settings.lua
@@ -1,7 +1,6 @@
 local g = vim.g
 
--- g.ale_linters = { python= { 'flake8', 'mypy', 'pylint', 'ruff', 'jedi' } }
-g.ale_linters = {python= {'ruff', 'flake8', 'pylint', 'mypy', 'pydocstring'}}
+g.ale_linters = {python= {'ruff', 'flake8', 'pylint', 'pydocstring'}}
 g.ale_fixers = {python= {'black', 'isort'}}
 g.ale_fix_on_save = 1
 -- Запуск линтера, только при сохранении
