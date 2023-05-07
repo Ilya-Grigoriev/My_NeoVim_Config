2fb09bdfd60e85eb5c3eec564a1141e3b2256a7a
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-05-07 13:22:38 +0300

    feat(plugin_config/ale_settings): added keymap for calling fixters

--- lua/plugin_config/ale_settings.lua
+++ lua/plugin_config/ale_settings.lua
@@ -4,9 +4,7 @@ g.ale_linters = {python= {'ruff', 'pyright'}}
 g.ale_fixers = {python= {'black', 'isort'}, javascript={'prettier'}, 
 html={'prettier'}, css={'prettier'}}
 
--- Запуск линтера, только при сохранении
-g.ale_fix_on_save = 1
-
+g.ale_fix_on_save = 0
 g.ale_echo_msg_format = '[%linter%] %s [%severity%]'
 g.ale_warn_about_trailing_whitespace = 0
 g.ale_virtualtext_cursor = 'disabled'
@@ -18,3 +16,4 @@ highlight clear ALEWarningSign
 g.ale_sign_error = ''
 g.ale_sign_warning = ''
 g.ale_linters_explicit = 1
+vim.keymap.set('n', '<S-f>', ':ALEFix black isort<CR>', {})
