69df845f45869315f35b3b4b7e364e55b660c474
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-06-02 12:16:38 +0300

    feat(plugin_config): added external file for configs of lsp servers

--- lua/plugin_config/lsp.lua
+++ lua/plugin_config/lsp.lua
@@ -2,7 +2,7 @@ require("mason").setup()
 require("mason-lspconfig").setup()
 
 vim.o.completeopt = 'menuone,noselect'
-vim.cmd[[lua vim.diagnostic.disable(0)]]
+vim.cmd [[lua vim.diagnostic.disable(0)]]
 
 local luasnip = require 'luasnip'
 local async = require "plenary.async"
@@ -12,73 +12,72 @@ local nvim_lsp = require('lspconfig')
 -- Use an on_attach function to only map the following keys
 -- after the language server attaches to the current buffer
 local on_attach = function(client, bufnr)
+    local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
+    local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
-  local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
+    -- Enable completion triggered by <c-x><c-o>
+    buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  -- Enable completion triggered by <c-x><c-o>
-  buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
+    -- Mappings.
+    local opts = { noremap = true, silent = true }
 
-  -- Mappings.
-  local opts = { noremap=true, silent=true }
+    -- See `:help vim.lsp.*` for documentation on any of the below functions
+    buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
+    buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
+    buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+    buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
+    buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
+    buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
+    buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
+    buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
+    buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
+    buf_set_keymap('n', ',ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
+    buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
+    buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
+    buf_set_keymap('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', opts)
+    buf_set_keymap('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', opts)
+    buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
+    buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.format{async=true}<CR>', opts)
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<space>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<space>wl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<space>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<space>rn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', ',ca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', '<space>e', '<cmd>lua vim.diagnostic.open_float()<CR>', opts)
-  buf_set_keymap('n', '[d', '<cmd>lua vim.lsp.diagnostic.goto_prev()<CR>', opts)
-  buf_set_keymap('n', ']d', '<cmd>lua vim.lsp.diagnostic.goto_next()<CR>', opts)
-  buf_set_keymap('n', '<space>q', '<cmd>lua vim.lsp.diagnostic.set_loclist()<CR>', opts)
-  buf_set_keymap('n', '<space>f', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
-
-  -- LSP Signature
-   cfg = {
-      debug = false, 
-      log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log", 
-      verbose = false, 
-      bind = true, 
-      doc_lines = 10, 
-      max_height = 12, 
-      max_width = 80, 
-      noice = false, 
-      wrap = true, 
-      floating_window_above_cur_line = true,
-      floating_window_off_x = 1, 
-      floating_window_off_y = 0,
-      close_timeout = 4000,
-      fix_pos = false,
-      hint_enable = false,
-      hint_scheme = "String",
-      hi_parameter = "LspSignatureActiveParameter", 
-      handler_opts = {
-        border = "rounded"   
-      },
-      always_trigger = false, 
-      auto_close_after = nil, 
-      extra_trigger_chars = {}, 
-      zindex = 200, 
-      padding = '', 
-      transparency = nil, 
-      shadow_blend = 36, 
-      shadow_guibg = 'Black', 
-      timer_interval = 200, 
-      toggle_key = nil, 
-      toggle_key_flip_floatwin_setting = false, 
-      select_signature_key = nil, 
-      move_cursor_key = nil, 
+    -- LSP Signature
+    cfg = {
+        debug = false,
+        log_path = vim.fn.stdpath("cache") .. "/lsp_signature.log",
+        verbose = false,
+        bind = true,
+        doc_lines = 10,
+        max_height = 12,
+        max_width = 80,
+        noice = false,
+        wrap = true,
+        floating_window_above_cur_line = true,
+        floating_window_off_x = 1,
+        floating_window_off_y = 0,
+        close_timeout = 4000,
+        fix_pos = false,
+        hint_enable = false,
+        hint_scheme = "String",
+        hi_parameter = "LspSignatureActiveParameter",
+        handler_opts = {
+            border = "rounded"
+        },
+        always_trigger = false,
+        auto_close_after = nil,
+        extra_trigger_chars = {},
+        zindex = 200,
+        padding = '',
+        transparency = nil,
+        shadow_blend = 36,
+        shadow_guibg = 'Black',
+        timer_interval = 200,
+        toggle_key = nil,
+        toggle_key_flip_floatwin_setting = false,
+        select_signature_key = nil,
+        move_cursor_key = nil,
     }
-    require'lsp_signature'.setup(cfg) 
-    require'lsp_signature'.on_attach(cfg, bufnr)
+    require 'lsp_signature'.setup(cfg)
+    require 'lsp_signature'.on_attach(cfg, bufnr)
 end
 
 -- TS setup
@@ -107,19 +106,21 @@ null_ls.setup({
     sources = {
         null_ls.builtins.diagnostics.eslint,
         null_ls.builtins.code_actions.eslint,
-        null_ls.builtins.formatting.prettier
+        null_ls.builtins.formatting.prettier,
+        null_ls.builtins.formatting.black
     },
     on_attach = on_attach
 })
 
+
 -- Stylelint format after save
-require'lspconfig'.stylelint_lsp.setup{
-  settings = {
-    stylelintplus = {
-      --autoFixOnSave = true,
-      --autoFixOnFormat = true,
+require 'lspconfig'.stylelint_lsp.setup {
+    settings = {
+        stylelintplus = {
+            --autoFixOnSave = true,
+            --autoFixOnFormat = true,
+        }
     }
-  }
 }
 
 
@@ -127,17 +128,17 @@ require'lspconfig'.stylelint_lsp.setup{
 -- map buffer local keybindings when the language server attaches
 local servers = { 'pyright', 'rust_analyzer', 'lua_ls' }
 for _, lsp in ipairs(servers) do
-  nvim_lsp[lsp].setup {
-    on_attach = on_attach,
-    flags = {
-      debounce_text_changes = 150,
+    nvim_lsp[lsp].setup {
+        on_attach = on_attach,
+        flags = {
+            debounce_text_changes = 150,
+        }
     }
-  }
 end
 
 vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
-  vim.lsp.diagnostic.on_publish_diagnostics, {
-    virtual_text = false,
-    signs = false
-  }
+    vim.lsp.diagnostic.on_publish_diagnostics, {
+        virtual_text = false,
+        signs = false
+    }
 )
---
+++ lua/plugin_config/lsp_servers.lua
@@ -0,0 +1,47 @@
+-- Setup language servers.
+local lspconfig = require('lspconfig')
+-- Config for LSP lua-language-server
+require'lspconfig'.lua_ls.setup {
+  settings = {
+    Lua = {
+      runtime = {
+        -- Tell the language server which version of Lua you're using (most likely LuaJIT in the case of Neovim)
+        version = 'LuaJIT',
+      },
+      diagnostics = {
+        -- Get the language server to recognize the `vim` global
+        globals = {'vim'},
+      },
+      workspace = {
+        -- Make the server aware of Neovim runtime files
+        library = vim.api.nvim_get_runtime_file("", true),
+      },
+      -- Do not send telemetry data containing a randomized but unique identifier
+      telemetry = {
+        enable = false,
+      },
+    },
+  },
+}
+
+-- Config for LSP pyright
+lspconfig.pyright.setup{
+    cmd={ "pyright-langserver", "--stdio" },
+    filetypes = {'python'},
+    settings = {
+        python = {
+            analysis = {
+                autoSearchPaths = true,
+                diagnosticMode = "workspace",
+                useLibraryCodeForTypes = true
+            }
+        }
+    }
+}
+
+-- Config for LSP rust
+lspconfig.rust_analyzer.setup {
+  settings = {
+    ['rust-analyzer'] = {},
+  },
+}
