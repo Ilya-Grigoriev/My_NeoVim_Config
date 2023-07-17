8b73a16374c3dea44893d969d188d178ed279e99
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-07-18 02:44:19 +0300

    fix(other_modules/surround_text): removed excess argument

--- lua/other_modules/surround_text.lua
+++ lua/other_modules/surround_text.lua
@@ -14,7 +14,7 @@ local template_lhs = 'yw%s'
 local template_rhs = 'c%s<Esc>pa%s<Esc>'
 
 for left_pair, right_pair in pairs(symbols_pairs) do
-    local lhs = string.format(template_lhs, left_pair, right_pair)
+    local lhs = string.format(template_lhs, left_pair)
     local rhs = string.format(template_rhs, left_pair, right_pair)
 
     vim.keymap.set('v', lhs, rhs, { noremap = true })
