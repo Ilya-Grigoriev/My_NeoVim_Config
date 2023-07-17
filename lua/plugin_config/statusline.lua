a70834e0f2c3b607279a554bbcf5e91074f2420a
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-07-17 23:55:44 +0300

    feat(plugin_config/statusline): added icons for lsp actions

--- lua/plugin_config/statusline.lua
+++ lua/plugin_config/statusline.lua
@@ -81,23 +81,19 @@ local function lsp()
     local info = ""
 
     if count["errors"] ~= 0 then
-        errors = " %#LspDiagnosticsSignError#  " .. count["errors"] .. ' '
+        errors = " %#LspDiagnosticsSignError# " .. count["errors"] .. ' '
     end
     if count["warnings"] ~= 0 then
-        warnings = " %#LspDiagnosticsSignWarning#  " .. count["warnings"] .. ' '
+        warnings = " %#LspDiagnosticsSignWarning# " .. count["warnings"] .. ' '
     end
     if count["hints"] ~= 0 then
-        hints = " %#LspDiagnosticsSignHint#  " .. count["hints"] .. ' '
+        hints = " %#LspDiagnosticsSignHint# " .. count["hints"] .. ' '
     end
     if count["info"] ~= 0 then
-        info = " %#LspDiagnosticsSignInformation#  " .. count["info"] .. ' '
+        info = " %#LspDiagnosticsSignInformation# " .. count["info"] .. ' '
     end
 
     return errors .. warnings .. hints .. info .. "%#Normal#"
-end
-
-local function filetype()
-    return string.format(" %s ", vim.bo.filetype)
 end
 
 local function lineinfo()
@@ -131,9 +127,8 @@ Statusline.active = function()
         filepath(),
         filename(),
         "%#Normal#",
-        lsp(),
         "%=%#StatusLineExtra#",
-        filetype(),
+        lsp(),
         lineinfo(),
     }
 end
