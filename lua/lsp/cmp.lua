550a40a394ed760d066d226f7038fc8f94447230
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-08-09 07:12:36 +0300

    feat(cmp): added autocompletion

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -1,8 +1,5 @@
 local cmp = require 'cmp'
 cmp.setup {
-    completion = {
-        autocomplete = false
-    },
     snippet = {
         expand = function(args)
             require('luasnip').lsp_expand(args.body)
@@ -19,24 +16,6 @@ cmp.setup {
             behavior = cmp.ConfirmBehavior.Replace,
             select = true,
         },
-        ['<Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            elseif luasnip.expand_or_jumpable() then
-                luasnip.expand_or_jump()
-            else
-                fallback()
-            end
-        end,
-        ['<S-Tab>'] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end,
     },
     sources = {
         { name = 'nvim_lsp' },
