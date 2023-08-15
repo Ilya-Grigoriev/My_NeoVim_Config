75fbc5f9767a47a4f2677889331d4da30f458e6b
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-08-15 17:27:44 +0300

    fix(telescope): removed excess lines

--- lua/plugin_configs/telescope.lua
+++ lua/plugin_configs/telescope.lua
@@ -1,9 +1,6 @@
-local telescope = require('telescope')
 local builtin = require('telescope.builtin')
 
 vim.keymap.set('n', 'ff', builtin.find_files, {})
 vim.keymap.set('n', 'fg', builtin.live_grep, {})
 vim.keymap.set('n', 'fb', builtin.buffers, {})
 vim.keymap.set('n', 'fh', builtin.help_tags, {})
-vim.keymap.set("n", "fr", telescope.extensions.recent_files.pick, {})
-vim.keymap.set('n', 'fc', builtin.colorscheme, {})
