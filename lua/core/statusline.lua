9eb7c23d04371dfef8532fd87bdfaf6f8fbce700
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-08-15 19:48:06 +0300

    fix

--- lua/core/statusline.lua
+++ lua/core/statusline.lua
@@ -74,7 +74,6 @@ vim.api.nvim_exec([[
   au!
   au WinEnter,BufEnter * setlocal statusline=%!v:lua.Statusline.active()
   au WinLeave,BufLeave * setlocal statusline=%!v:lua.Statusline.inactive()
-  au WinEnter,BufEnter,FileType NvimTree setlocal
-  \. statusline=%!v:lua.Statusline.short()
+  au WinEnter,BufEnter,FileType NvimTree setlocal statusline=%!v:lua.Statusline.short()
   augroup END
 ]], false)
