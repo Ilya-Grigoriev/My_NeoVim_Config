489cb4e0ab02f9a6c9463733589c7eee8dfb85be
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-08-30 14:13:33 +0300

    feat(keymaps): added keymap for selecting all lines

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,5 +1,5 @@
 vim.g.mapleader = ","
-vim.cmd [[set colorcolumn=79]]
+vim.cmd [[set colorcolumn=81]]
 -- vim.api.nvim_set_keymap('n', 'x', '"xx', {noremap = true})
 -- vim.api.nvim_set_keymap('n', 'dd', '"ddd', {noremap = true})
 vim.api.nvim_set_keymap('n', ',<Space>', ':nohlsearch<CR>', {})
@@ -12,7 +12,7 @@ vim.api.nvim_set_keymap("n", "<leader>rn",
 vim.api.nvim_set_keymap('n', '<S-k>', '<cmd> lua vim.lsp.buf.hover()<CR>',
     { noremap = true })
 vim.api.nvim_set_keymap('n', '<leader>rf', ':call delete(expand(""))', {})
-vim.api.nvim_set_keymap('n', '<leader>ps', ':py3file %<CR>', {})
+vim.api.nvim_set_keymap('n', '<leader>ps', ':py5file %<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', {})
 vim.api.nvim_set_keymap('n', '<leader>rg', ':Rg ', {})
 vim.api.nvim_set_keymap('n', '<leader>s', ':%s/', {})
@@ -24,10 +24,13 @@ vim.keymap.set('n', ',bf', ':bd ', {})
 vim.api.nvim_set_keymap("t", '<C-t>', '<C-\\><C-n>', { noremap = true })
 vim.keymap.set('n', 'tn', ':tabnew<CR>', { noremap = true })
 vim.keymap.set('n', ',ch', ":call histdel(':')<CR>", {})
-vim.keymap.set('n', '<C-Up>', ':m -2<CR>', { noremap = true })
-vim.keymap.set('n', '<C-Down>', ':m +1<CR>', { noremap = true })
+vim.keymap.set('n', '<C-Up>', ':m 0<CR>', { noremap = true })
+vim.keymap.set('n', '<C-Down>', ':m +3<CR>', { noremap = true })
 vim.keymap.set('v', '<C-y>', '"+y', { noremap = true })
 
 -- Tabs
 vim.keymap.set('n', 'L', 'gt', { noremap = true })
 vim.keymap.set('n', 'H', 'gT', { noremap = true })
+
+-- Other
+vim.keymap.set('n', '<C-a>', 'ggVG', { noremap = true })
