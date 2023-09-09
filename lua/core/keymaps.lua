4239557394d511158e6c0fd48b2bd1234e9ef46d
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-10 00:17:51 +0300

    refactor(keymaps): divided code into blocks with comments

--- lua/core/keymaps.lua
+++ lua/core/keymaps.lua
@@ -1,36 +1,51 @@
 vim.g.mapleader = ","
-vim.cmd [[set colorcolumn=81]]
--- vim.api.nvim_set_keymap('n', 'x', '"xx', {noremap = true})
--- vim.api.nvim_set_keymap('n', 'dd', '"ddd', {noremap = true})
-vim.api.nvim_set_keymap('n', ',<Space>', ':nohlsearch<CR>', {})
-vim.api.nvim_set_keymap('i', 'jk', '<ESC>', {})
-vim.api.nvim_set_keymap('n', '<C-/>', ':NERDCommenterToggle<CR>', {})
-vim.api.nvim_set_keymap('n', '<C-[>', '<C-w><Left>', {})
-vim.api.nvim_set_keymap('n', '<C-]>', '<C-w><Right>', {})
-vim.api.nvim_set_keymap("n", "<leader>rn",
+
+-- Exiting to Normal mode
+vim.keymap.set('i', 'jk', '<ESC>', {})
+vim.keymap.set("t", '<C-t>', '<C-\\><C-n>', { noremap = true })
+
+vim.keymap.set('n', ',<Space>', ':nohlsearch<CR>', {})
+
+-- NerdTree
+vim.keymap.set('n', '<C-/>', ':NERDCommenterToggle<CR>', {})
+vim.keymap.set('n', '<C-[>', '<C-w><Left>', {})
+vim.keymap.set('n', '<C-]>', '<C-w><Right>', {})
+
+-- LSP
+vim.keymap.set("n", "<leader>rn",
     "<cmd>lua vim.lsp.buf.rename()<CR>", { noremap = true })
-vim.api.nvim_set_keymap('n', '<S-k>', '<cmd> lua vim.lsp.buf.hover()<CR>',
+vim.keymap.set('n', '<S-k>', '<cmd> lua vim.lsp.buf.hover()<CR>',
     { noremap = true })
-vim.api.nvim_set_keymap('n', '<leader>rf', ':call delete(expand(""))', {})
-vim.api.nvim_set_keymap('n', '<leader>ps', ':py3file %<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>tn', ':tabnew<CR>', {})
-vim.api.nvim_set_keymap('n', '<leader>rg', ':Rg ', {})
-vim.api.nvim_set_keymap('n', '<leader>s', ':%s/', {})
-vim.api.nvim_set_keymap('x', '<leader>s', ':s/', {})
-vim.api.nvim_set_keymap('n', '<C-b>', '<C-^><CR>', {})
-vim.api.nvim_set_keymap('n', ',bs', ':buffers<CR>', {})
-vim.api.nvim_set_keymap('n', ',bd', ':%bd<CR>', {})
-vim.keymap.set('n', ',bf', ':bd ', {})
-vim.api.nvim_set_keymap("t", '<C-t>', '<C-\\><C-n>', { noremap = true })
+
+vim.keymap.set('n', '<leader>rf', ':call delete(expand(""))', {})
+
+-- Run python file
+vim.keymap.set('n', '<leader>ps', ':py3file %<CR>', {})
+
+-- Tabs
+vim.keymap.set('n', '<leader>tn', ':tabnew<CR>', {})
 vim.keymap.set('n', 'tn', ':tabnew<CR>', { noremap = true })
-vim.keymap.set('n', ',ch', ":call histdel(':')<CR>", {})
+
+-- Ripgrep
+vim.keymap.set('n', '<leader>rg', ':Rg ', {})
+
+-- Substitution
+vim.keymap.set('n', '<leader>s', ':%s/', {})
+vim.keymap.set('x', '<leader>s', ':s/', {})
+
+-- Buffers
+vim.keymap.set('n', ',bs', ':buffers<CR>', {})
+vim.keymap.set('n', ',bd', ':%bd<CR>', {})
+vim.keymap.set('n', ',bf', ':bd ', {})
+
+
 vim.keymap.set('v', '<C-y>', '"+y', { noremap = true })
 
 -- Move line
 vim.keymap.set('n', '<C-Up>', ':m -2<CR>', { noremap = true })
 vim.keymap.set('n', '<C-Down>', ':m +1<CR>', { noremap = true })
 
--- Tabs
+-- Moving tabs
 vim.keymap.set('n', 'L', 'gt', { noremap = true })
 vim.keymap.set('n', 'H', 'gT', { noremap = true })
 
