8e04d2e10b768ab050cddf6d24c8ad38700939fe
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-13 22:56:35 +0300

    fix(treesitter)

--- lua/plugin_configs/_treesitter.lua
+++ lua/plugin_configs/_treesitter.lua
@@ -2,7 +2,7 @@ require 'nvim-treesitter.configs'.setup {
     ensure_installed = { "c", "lua", "vim", "vimdoc", "query", 'python' },
 
     sync_install = false,
-    auto_install = true,
+    auto_install = false,
 
     highlight = {
         enable = true,
