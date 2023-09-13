d57027a3b22520d3171b8b994a7db3f25ecd1a23
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-13 21:59:10 +0300

    style(settings)

--- lua/core/settings.lua
+++ lua/core/settings.lua
@@ -1,15 +1,21 @@
 local o = vim.o
 
 o.number = true
+
 o.shiftwidth = 4
 o.tabstop = 4
+vim.cmd [[set colorcolumn=81]]
+
 o.encoding = "UTF-8"
+
 o.mouse = "a"
+
 o.title = true
 o.softtabstop = 4
-o.textwidth = 79
-o.autoindent = true
 o.expandtab = true
+
+o.autoindent = true
+
 o.cursorline = true
+
 o.scrolloff = 5
-vim.cmd [[set colorcolumn=81]]
