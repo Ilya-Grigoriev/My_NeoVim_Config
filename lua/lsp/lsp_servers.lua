a53c7ab3f300d013a95dd976fbf7efc75f21f4b3
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-21 21:42:49 +0300

    feat(lsp_servers): added neodev config

--- lua/lsp/lsp_servers.lua
+++ lua/lsp/lsp_servers.lua
@@ -1,26 +1,39 @@
 -- Setup language servers.
 local lspconfig = require('lspconfig')
-local util = require 'lspconfig.util'
+local util = require('lspconfig.util')
 
 -- Config for LSP lua-language-server
-require 'lspconfig'.lua_ls.setup {
+require("neodev").setup({
+    -- add any options here, or leave empty to use the default settings
+})
+
+lspconfig.lua_ls.setup {
     on_init = function(client)
         local path = client.workspace_folders[1].name
-        if not vim.loop.fs_stat(path .. '/.luarc.json')
-            and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
-            client.config.settings = vim.tbl_deep_extend('force',
-                client.config.settings.Lua, {
+        if not vim.loop.fs_stat(path .. '/.luarc.json') and not vim.loop.fs_stat(path .. '/.luarc.jsonc') then
+            client.config.settings = vim.tbl_deep_extend('force', client.config.settings, {
+                Lua = {
                     runtime = {
                         version = 'LuaJIT'
                     },
-                    workspace = {
-                        library = { vim.env.VIMRUNTIME }
-                    }
-                })
+                    completion = {
+                        callSnippet = "Replace"
+                    },
 
-            client.notify("workspace/didChangeConfiguration",
-                { settings = client.config.settings }
-            )
+                    workspace = {
+                        checkThirdParty = false,
+                        library = {
+                            vim.env.VIMRUNTIME
+                            -- "${3rd}/luv/library"
+                            -- "${3rd}/busted/library",
+                        }
+                        -- or pull in all of 'runtimepath'. NOTE: this is a lot slower
+                        -- library = vim.api.nvim_get_runtime_file("", true)
+                    }
+                }
+            })
+
+            client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
         end
         return true
     end
@@ -111,6 +124,31 @@ lspconfig.taplo.setup {
 }
 
 -- Config for LSP xml-files
-require 'lspconfig'.lemminx.setup {
+lspconfig.lemminx.setup {
     filetypes = { 'xml' }
+}
+
+-- Config for LSP vimls
+lspconfig.vimls.setup {
+    cmd = { "vim-language-server", "--stdio" },
+    filetypes = { "vim" },
+    init_options = {
+        diagnostic = {
+            enable = true
+        },
+        indexes = {
+            count = 3,
+            gap = 100,
+            projectRootPatterns = { "runtime", "nvim", ".git", "autoload", "plugin" },
+            runtimepath = true
+        },
+        isNeovim = true,
+        iskeyword = "@,48-57,_,192-255,-#",
+        runtimepath = "",
+        suggest = {
+            fromRuntimepath = true,
+            fromVimruntime = true
+        },
+        vimruntime = ""
+    }
 }
