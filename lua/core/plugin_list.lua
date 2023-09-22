333e70a92568151461d28f5f844de3b3b815ba5c
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-22 22:53:36 +0300

    feat(plugin_list): added which-key plugin

--- lua/core/plugin_list.lua
+++ lua/core/plugin_list.lua
@@ -83,5 +83,19 @@ return {
     },
 
     -- LSP for java
-    'mfussenegger/nvim-jdtls'
+    'mfussenegger/nvim-jdtls',
+
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        init = function()
+            vim.o.timeout = true
+            vim.o.timeoutlen = 300
+        end,
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    }
 }
