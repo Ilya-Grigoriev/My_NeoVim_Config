bf2b4f60eaf056d0875dbc30bdc6635a0f7324ff
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-24 17:30:27 +0300

    delete(which-key)

--- init.lua
+++ init.lua
@@ -25,7 +25,6 @@ require("plugin_configs/gitsigns")
 require("plugin_configs/snippets")
 require("plugin_configs/harpoon")
 require("plugin_configs/_treesitter")
-require('plugin_configs/which_keymaps')
 
 -- other modules
 require("other_modules/surround_text")
