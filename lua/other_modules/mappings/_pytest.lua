e33380f0f8612f59b7971cebef1ec07919559636
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-09-29 21:30:49 +0300

    feat(keymaps): added keymaps for running pytest

---
+++ lua/other_modules/mappings/_pytest.lua
@@ -0,0 +1,13 @@
+vim.keymap.set('n', ',pa', function()
+    vim.cmd('split')
+    vim.cmd('horizontal resize 7')
+    vim.cmd.terminal('pytest')
+    vim.cmd.startinsert()
+end, { desc = 'Run pytest for all tests' })
+
+vim.keymap.set('n', ',pf', function()
+    vim.cmd('split')
+    vim.cmd('horizontal resize 7')
+    vim.cmd.terminal('pytest %')
+    vim.cmd.startinsert()
+end, { desc = 'Run pytest for current file' })
