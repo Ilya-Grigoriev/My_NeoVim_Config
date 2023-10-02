f3154503473d21d61a7e87b257b95cd5cff1c957
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-02 13:54:03 +0300

    feat(switch_tabs): added moving between tabs in Terminal mode

--- lua/other_modules/mappings/switch_tabs.lua
+++ lua/other_modules/mappings/switch_tabs.lua
@@ -5,4 +5,10 @@ for n_tab = 1, 10, 1 do
         string.format('%dgt', n_tab),
         { desc = 'Switch tabs by using Alt+{number}' }
     )
+    vim.keymap.set(
+        't',
+        string.format('<M-%d>', n_tab),
+        string.format('<C-\\><C-n>%dgt', n_tab),
+        { desc = 'Switch tabs by using Alt+{number} in terminal' }
+    )
 end
