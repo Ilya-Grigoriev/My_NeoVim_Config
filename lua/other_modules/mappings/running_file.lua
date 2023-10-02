dd3e7dc5acff9a6e86917462d8b6701875a83976
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-02 22:08:57 +0300

    fix(mappings): removed symbols i for go to Insert mode

--- lua/other_modules/mappings/running_file.lua
+++ lua/other_modules/mappings/running_file.lua
@@ -1,6 +1,6 @@
 local commands_filetypes = {
-    python = ':term python3 %<CR>i',
-    lua = ':term lua %<CR>i',
+    python = ':term python3 %<CR>',
+    lua = ':term lua %<CR>',
 }
 
 for filetype, command_for_run in pairs(commands_filetypes) do
