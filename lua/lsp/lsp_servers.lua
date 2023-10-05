4355293cb723f259f5293c0f4a805f4cfb866b35
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-05 15:44:02 +0300

    refactor(lsp_servers): added checking for lsp availability

--- lua/lsp/lsp_servers.lua
+++ lua/lsp/lsp_servers.lua
@@ -1,8 +1,17 @@
--- Setup language servers.
-require('lsp/servers/_lua')
-require('lsp/servers/_python')
-require('lsp/servers/_rust')
-require('lsp/servers/_cpp')
-require('lsp/servers/_toml')
-require('lsp/servers/_xml')
-require('lsp/servers/_vim')
+local utils = require('core/utils')
+
+local filename_to_lsp_name = {
+    ['lsp/servers/_lua'] = 'lua-language-server',
+    ['lsp/servers/_python'] = 'pyright',
+    ['lsp/servers/_rust'] = 'rust_analyzer',
+    ['lsp/servers/_cpp'] = 'clangd',
+    ['lsp/servers/_toml'] = 'taplo',
+    ['lsp/servers/_xml'] = 'lemminx',
+    ['lsp/servers/_vim'] = 'vimls'
+}
+
+for filename, lsp_name in pairs(filename_to_lsp_name) do
+    if utils.executable(lsp_name) then
+        require(filename)
+    end
+end
