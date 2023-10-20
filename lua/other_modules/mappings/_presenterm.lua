1c3b973df56d64c5cb594c3f0c92d36fde5ca38b
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-21 00:36:43 +0300

    feat: added keymaps for work with presenterm tool

---
+++ lua/other_modules/mappings/_presenterm.lua
@@ -0,0 +1,38 @@
+vim.api.nvim_create_autocmd("FileType", {
+    pattern = "markdown",
+    callback = function(args)
+        vim.keymap.set('n', 'ps', ':vsplit | term presenterm %<CR>', { desc = "Run presenterm", buffer = args.buf })
+    end,
+})
+
+vim.api.nvim_create_autocmd(
+    { 'TermOpen' },
+    {
+        pattern = 'term://*presenterm*',
+        callback = function(_)
+            vim.cmd('set showtabline=0')
+            vim.cmd('set nonumber')
+            vim.cmd('set noshowcmd')
+            vim.cmd('set noruler')
+            vim.cmd('set noshowmode')
+            vim.cmd('set laststatus=0')
+            vim.cmd('call feedkeys("\\<C-\\>\\<C-n>\\<CR>i")')
+        end
+    }
+)
+
+vim.api.nvim_create_autocmd(
+    { 'TermClose' },
+    {
+        pattern = 'term://*presenterm*',
+        callback = function(args)
+            vim.cmd('set showtabline=1')
+            vim.cmd('set number')
+            vim.cmd('set showmode')
+            vim.cmd('set showcmd')
+            vim.cmd('set ruler')
+            vim.cmd('set laststatus=2')
+            vim.cmd(string.format('e %s', args['file']))
+        end
+    }
+)
