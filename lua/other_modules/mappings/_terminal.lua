bbf51b258451850dd4b45d57fec01e87c424c829
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-21 00:33:52 +0300

    feat(terminal): added ability to auto close window with message "Process exited"

--- lua/other_modules/mappings/_terminal.lua
+++ lua/other_modules/mappings/_terminal.lua
@@ -13,13 +13,21 @@ vim.api.nvim_create_autocmd(
     }
 )
 
+vim.api.nvim_create_autocmd('TermClose',
+    {
+        callback = function()
+            vim.cmd('call feedkeys("\\<CR>")')
+        end
+    }
+)
+
 vim.keymap.set("t", '<C-t>', '<C-\\><C-n>',
     {
         noremap = true,
         desc = "Exiting from Terminal mode to Normal mode"
     }
 )
-vim.keymap.set('t', '<C-q>', '<C-\\><C-n>:bd!<CR>', { desc = 'Close terminal' })
+vim.keymap.set('t', '<C-q>', '<C-\\><C-n> | :q!<CR>', { desc = 'Close terminal' })
 
 vim.keymap.set('t', '<C-h>', '<C-\\><C-n><C-w>h<CR>', { desc = 'Moved to right split in Terminal mode' })
 vim.keymap.set('t', '<C-l>', '<C-\\><C-n><C-w>l<CR>', { desc = 'Moved to left split in Terminal mode' })
