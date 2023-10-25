e1483ff0f877905fec8531e49ca40e28e8f7d607
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-10-25 23:28:01 +0300

    fix(neogen): removed keymaps for moving

--- lua/plugin_configs/_neogen.lua
+++ lua/plugin_configs/_neogen.lua
@@ -12,32 +12,4 @@ neogen.setup {
     }
 }
 
-local cmp = require('cmp')
-
-cmp.setup {
-    -- You must set mapping if you want.
-    mapping = {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-            if neogen.jumpable() then
-                neogen.jump_next()
-            else
-                fallback()
-            end
-        end, {
-            "i",
-            "s",
-        }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-            if neogen.jumpable(true) then
-                neogen.jump_prev()
-            else
-                fallback()
-            end
-        end, {
-            "i",
-            "s",
-        }),
-    },
-}
-
 vim.keymap.set('n', ',d', ":lua require('neogen').generate()<CR>", {})
