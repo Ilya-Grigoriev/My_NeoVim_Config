2a2da272e056caa94c6a921edeb0a93b19c70d26
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-11-10 07:15:16 +0300

    refactor(terminal): renamed file, removed enter CR when `TerminalClose` event is calling

---
+++ plugin/terminal.lua
@@ -0,0 +1,21 @@
+vim.keymap.set('n', '<C-\\>', function()
+    vim.cmd.tabnew()
+    vim.cmd.terminal()
+end, { desc = 'Open terminal in new tab' })
+
+vim.api.nvim_create_autocmd(
+    { 'BufEnter', 'TermOpen' },
+    {
+        pattern = 'term://*',
+        callback = function(_)
+            vim.cmd.startinsert()
+        end
+    }
+)
+
+vim.keymap.set("t", '<C-t>', '<C-\\><C-n>',
+    {
+        noremap = true,
+        desc = "Exiting from Terminal mode to Normal mode"
+    }
+)
