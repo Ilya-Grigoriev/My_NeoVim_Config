0462bc8600604dfbc783db4f17c987bd1c33b8d2
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-11-21 07:06:20 +0300

    feat(running_file): added horizontal split

--- plugin/running_file.lua
+++ plugin/running_file.lua
@@ -1,3 +1,5 @@
+local running_file = vim.api.nvim_create_augroup('RunningFIle', { clear = true })
+
 local commands_filetypes = {
     python = 'python %s',
     lua = 'lua %s',
@@ -9,15 +11,16 @@ for filetype, command_for_run in pairs(commands_filetypes) do
     vim.api.nvim_create_autocmd('FileType',
         {
             pattern = filetype,
+            group = running_file,
             callback = function(args)
                 vim.keymap.set('n', '<CR>',
                     function()
                         filepath = vim.fn.expand('%')
                         result_command = string.format(command_for_run, filepath)
 
-                        vim.cmd.tabnew()
+                        vim.cmd.split()
                         vim.cmd.terminal(result_command)
-                    end, {}
+                    end, { desc = string.format('Run %s file', filetype) }
                 )
             end
         }
