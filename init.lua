1fc01942d43cd8e5707879cd1a529511e3c1b73b
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-11-21 07:05:01 +0300

    fix(init): removed requiring plugins

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 require('core/health')
 require("core/settings")
 require("core/keymaps")
-require("core/plugins")
+require("core/lazy")
 
 -- lsp
 require("lsp/_lsp_signature")
@@ -11,22 +11,3 @@ require("lsp/_null_ls")
 require("lsp/cmp")
 require("lsp/lsp")
 require("lsp/lsp_servers")
-
--- plugin configs
-require("plugin_configs/telescope")
-require("plugin_configs/neovimtree")
-require("plugin_configs/commenter")
-require("plugin_configs/webdevicons")
-require('plugin_configs/_neogen')
-require("plugin_configs/gitsigns")
-require("plugin_configs/snippets")
-require("plugin_configs/_treesitter")
-
--- other modules
-require("other_modules/mappings/surround_text")
-require('other_modules/mappings/git')
-require('other_modules/_python_venv')
-require('other_modules/mappings/_presenterm')
-
-vim.cmd.colorscheme('_statusline')
-vim.cmd.colorscheme('gruvbox')
