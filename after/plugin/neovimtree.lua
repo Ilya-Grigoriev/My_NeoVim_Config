77d06f61e0435f826c7a6aa2d3fa2abeb52d9cdb
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-11-25 13:44:30 +0300

    feat(nvimtree): added opportunity of using netrw

--- after/plugin/neovimtree.lua
+++ after/plugin/neovimtree.lua
@@ -2,10 +2,16 @@ vim.g.nvim_tree_auto_open = 1
 vim.g.nvim_tree_width = 25
 vim.g.nvim_tree_auto_close = 1
 vim.g.nvim_tree_indent_markers = 1
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
--- set termguicolors to enable highlight groups
+-- If you want to use netrw
+-- vim.g.netrw_banner = 0
+-- vim.g.netrw_browse_split = 3
+-- vim.g.netrw_altv = 1
+-- vim.g.netrw_lifestyle = 3
+
 vim.opt.termguicolors = true
 
 require("nvim-tree").setup({
