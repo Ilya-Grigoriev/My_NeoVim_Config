9ec3bdc2ec70044bd36ee343dbe3f31af51dd3c0
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2023-12-02 23:26:07 +0300

    refactor(markdown): added functions

--- plugin/markdown.lua
+++ plugin/markdown.lua
@@ -1,5 +1,61 @@
 local markdown = vim.api.nvim_create_augroup('MarkdownFileSettings', { clear = true })
 
+local types_convert = {
+    ['1'] = 'pdf',
+    ['2'] = 'latex',
+}
+
+
+---@return number
+local function _ask_converting_type()
+    local type_convert
+    local prompt = 'Convert to (enter number): \n 1: PDF\n 2: LaTeX\nYour choice: '
+
+    vim.ui.input(
+        {
+            prompt = prompt
+        },
+        function(input)
+            type_convert = input
+        end
+    )
+    return type_convert
+end
+
+local function _convert_md()
+    local number_convert = _ask_converting_type()
+
+    if number_convert ~= '1' and number_convert ~= '2' then
+        error('Incorrect choice')
+    end
+
+    type_convert = types_convert[number_convert]
+
+    filename = vim.fn.expand('%')
+
+    filename_without_extension = vim.fn.expand('%:r')
+
+    pdf_filename = filename_without_extension .. '.' .. type_convert
+
+    vim.cmd.terminal(
+        string.format(
+            'pandoc --quiet --from=gfm --to=%s -o %s %s',
+            type_convert,
+            pdf_filename,
+            filename)
+    )
+end
+
+local function _open_md()
+    filename_without_extension = vim.fn.expand('%:r')
+    pdf_filename = filename_without_extension .. '.pdf'
+
+    vim.cmd.tabnew()
+    vim.cmd.terminal(string.format('okular %s', pdf_filename))
+    vim.cmd.tabprev()
+    vim.cmd [[call feedkeys("\<Esc>")]]
+end
+
 vim.api.nvim_create_autocmd('FileType',
     {
         pattern = 'markdown',
@@ -7,29 +63,10 @@ vim.api.nvim_create_autocmd('FileType',
         desc = 'Convert Markdown file to PDF and open Markdown in PDF',
         callback = function(args)
             vim.keymap.set('n', ',mc',
-                function()
-                    filename = vim.fn.expand('%')
-
-                    filename_without_extension = vim.fn.expand('%:r')
-                    pdf_filename = filename_without_extension .. ".pdf"
-
-                    vim.cmd.terminal(
-                        string.format(
-                            'pandoc --quiet --from=gfm --to=pdf -o %s %s',
-                            pdf_filename,
-                            filename)
-                    )
-                end, { desc = 'Export markdown to pdf' }
+                _convert_md, { desc = 'Export markdown to PDF/LaTeX' }
             )
             vim.keymap.set('n', ',mo',
-                function()
-                    filename_without_extension = vim.fn.expand('%:r')
-                    pdf_filename = filename_without_extension .. '.pdf'
-
-                    vim.cmd.tabnew()
-                    vim.cmd.terminal(string.format('okular %s', pdf_filename))
-                    vim.cmd.tabprev()
-                end,
+                _open_md,
                 { desc = "Open Markdown in PDF (PDF file must be)" }
             )
         end
