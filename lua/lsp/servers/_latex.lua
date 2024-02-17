9d7d410fa06ff2802badf680e3ea4ce58129ffc4
Ilya Grigoryev <iliagrigoryev@yandex.ru>
2024-02-17 14:05:17 +0300

    fix(lsp:servers): changed LSP for latex

--- lua/lsp/servers/_latex.lua
+++ lua/lsp/servers/_latex.lua
@@ -1,12 +1,5 @@
 local lspconfig = require('lspconfig')
 
-lspconfig.ltex.setup {
-    cmd = { "ltex-ls" },
-    filetypes = { "text", "plaintex", "tex", "markdown" },
-    settings = {
-        ltex = {
-            language = "ru"
-        },
-    },
-    flags = { debounce_text_changes = 300 },
+lspconfig.texlab.setup {
+    cmd = { "texlab" },
 }
