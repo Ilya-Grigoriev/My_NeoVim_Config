4e471f2a3b869c361513ac85f96178ec8b4bbdda
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-07-24 21:24:26 +0300

    feat(cmp): disabled rounded for windows

--- lua/lsp/cmp.lua
+++ lua/lsp/cmp.lua
@@ -7,10 +7,6 @@ cmp.setup({
             require("luasnip").lsp_expand(args.body)
         end,
     },
-    window = {
-        completion = cmp.config.window.bordered(),
-        documentation = cmp.config.window.bordered(),
-    },
     mapping = cmp.mapping.preset.insert({
         ["<C-y>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
