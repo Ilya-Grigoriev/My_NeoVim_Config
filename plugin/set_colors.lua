970510a4a1ad11dbdb6429f9c9e2bbba702cf77d
Ilya Grigoriev <iliagrigoryev@yandex.ru>
2024-08-24 21:13:46 +0300

    feat(colors): disabled background

--- plugin/set_colors.lua
+++ plugin/set_colors.lua
@@ -1,2 +1,10 @@
 vim.cmd.colorscheme('_statusline')
 vim.cmd.colorscheme('kanagawa-dragon')
+vim.cmd
+[[
+hi Normal guibg=NONE ctermbg=NONE
+hi NonText guibg=NONE ctermbg=NONE
+hi LineNr guibg=NONE ctermbg=NONE
+hi StatusLine guibg=NONE ctermbg=NONE ctermfg=white guifg=white
+hi SignColumn guibg=NONE ctermbg=NONE
+]]
